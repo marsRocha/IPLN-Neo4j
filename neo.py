f7099b11b5bf99f37193843e20f63590220f570b
Marcio Rocha <marciosr@live.com.pt>
2019-11-28 02:41:45 +0000

    Relation func

--- neo.py
+++ neo.py
@@ -7,6 +7,8 @@ import fileinput
 from neo4j import GraphDatabase #import of neo4j
 
 
+
+
 def formatText(text):
 
     listPhrases = []
@@ -33,11 +35,23 @@ def addRelation(session,person1, person2, relation):
     session.run("MERGE (p1:Person {surename: $person1}) RETURN p1.surename",person1 = person1)
     session.run("MERGE (p2:Person {surename: $person2}) RETURN p2.surename",person2 = person2)
     #adds relation to nodes
+    print(person1+" "+ person2 +" "+ relation)
+
     session.run("MATCH(p1:Person),(p2:Person) WHERE p1.surename = $person1 AND p2.surename = $person2 CREATE (p1)-[:"+relation+"]->(p2)", person1 = person1, person2 = person2)
 
-def getPerson(name, people):
-    p = people.get(name)
-    return p
+def getRelation(phrase):
+    relation = ""
+    lookups = [
+        ('cas(?:ou|ado)', "casado"),
+        ('não são casados', "divorciado"),
+    ]
+
+    for pattern, value in lookups:
+        if re.search(pattern, phrase):
+            relation = value
+
+    return relation
+
 
 def main():
     file = sys.argv[1]
@@ -51,41 +65,36 @@ def main():
     for line in fileinput.input(file):
         text += line
 
-    
     lastPerson = "" 
-
     phrases = []
-
     phrases = formatText(text)
-
     p = []
     pM = r"(?:[A-ZÀ-Ü]\w+|[A-ZÀ-Ü]\.|[A-ZÀ-Ü][a-z]\.)"
-    rS = r"(?:casado|casou|divorciado|divorciou|namorado|namorada|esposa|marido)"
+    rS = r"(?:casado|casou|divorciado|divorciou|namorado|namorada|esposa|marido|não são casados)"
 
+    #Equitecar nomes e relationStatus em cada frase
     for phrase in phrases:
-        #Equitecar nomes e relationStatus
         ph = re.sub(r"(?:o|a|O|A|do|da) " + f"({pM})", r'{\1}:p', phrase)
         ph = re.sub(f" ({rS})", r'{\1}:r', ph)
         p.append(ph)
+        #print(p)
 
     for phrase in p:
         if(re.search(r"{(.*?)}:r", phrase)):
-            relation = re.findall(r"{(\w+)}:r", phrase)
+            #relation = re.findall(r"{(\w+)}:r", phrase)
+            relation = getRelation(phrase)
             if(re.search(r"(.*?):p", phrase)):
                 result = re.findall(r"{(\w+)}:p",phrase)
                 if(len(result) > 1):
-                    
-                    addRelation(session, result[0], result[1], relation[0])     
+                    addRelation(session, result[0], result[1], relation)     
                 lastPerson = result[len(result) - 1]    
-#print(p)
 
 
 
 
-    nome = "Kermit"
+    #nome = "Kermit"
     #createNode(session,nome,nodeperson)
     #addRelation(session,"Tell ",nome,"daddychocked")
-
 
 if __name__ == "__main__":
     main()
