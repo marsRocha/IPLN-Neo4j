18a601b2679b63e9a2e423cc947dfe3a4fac5fae
Marcio Rocha <marciosr@live.com.pt>
2019-12-01 04:10:53 +0000

    Final code

--- neo.py
+++ neo.py
@@ -4,9 +4,10 @@ import re
 import subprocess
 import sys
 import fileinput 
-from neo4j import GraphDatabase #import of neo4j
-import nltk
-from nltk.tokenize import word_tokenize
+from neo4j import GraphDatabase
+import os.path #used to read files
+import requests as req #use to establish connection with web
+from getopt import getopt
 
 
 def formatText(text):
@@ -24,7 +25,7 @@ def formatText(text):
 
     return phrases
 
-#neo4j functions
+#creates a node if it does not exist
 def createNode(driver,person):
     with driver.session() as session:
         session.run("MERGE (p:Person {surename: $person}) RETURN p.surename", person = person)
@@ -34,66 +35,106 @@ def addRelation(driver,person1, person2, relation):
     with driver.session() as session:
         session.run("MATCH (p1:Person { surename: $person1 }),(p2:Person { surename: $person2 }) MERGE (p1)-[r:"+relation +"]->(p2) RETURN p1.surename, type(r), p2.surename", person1 = person1, person2 = person2, relation = relation)
 
+#clears all data from database
 def cleanTable(driver):
     with driver.session() as session:
         session.run("MATCH (n) DETACH DELETE n")
 
+#returns relation depending on received regex
 def getRelation(phrase):
     relation = ""
     lookups = [
-        ('cas(?:ou|ado)', "casado"),
+        ('cas(?:ou|ado|ada|aram)', "casado"),
         ('não são casados', "divorciado"),
-        ('namora', "namora"),
+        ('divorci(?:ado|ados|ada|ou|aram)',"divorciado"),
+        ('namor(?:a|ado|ada)', "namora"),
+        ('namor(?:ou|aram)',"exnamorado"),
+        ('irmãos', "irmão"),
+        ('esposa',"esposa"),
+        ('marido',"marido"),
     ]
 
     for pattern, value in lookups:
         if re.search(pattern, phrase):
             relation = value
-
     return relation
 
-    
+#creates list of names
+def createNameList(texto):
+    if not os.path.exists('names.txt'):
+        f = open("names.txt", "w")
+        x = req.get("https://pt.wikipedia.org/wiki/Lista_de_nomes_portugueses")
+        text = x.text
+        names = re.findall(r"<i>infopedia</i> ([A-ZÀ-Ü][a-zà-ü]*)</a>.</span>", text)
+        for n in re.findall(r"<li>([A-ZÀ-Ü][a-zà-ü]*)</li>", text):
+            if (len(n) > 1):
+                if (n not in names):
+                    names.append(n)
+
+        for n in (re.findall(r"<li>([A-ZÀ-Ü][a-zà-ü]*)<sup id=\"cite_ref-(?:.*?)\" class=\"reference\">", text)):
+            if (len(n) > 1):
+                if (n not in names):
+                    names.append(n)
+        for n in names:
+            f.write(n + "\n")
+            f.wr
+        f.close()
+    else:
+        for line in fileinput.input("names.txt"):
+            nameList.append(line)
+
+#main function
 def main():
-    file = sys.argv[1]
+    opts, resto = getopt(sys.argv[1:], "fc")
+    dop = dict(opts)
 
     #Initiate connection and create session
-    graphdb = GraphDatabase.driver(uri="bolt://localhost:7687" , auth=("neo4j", "eheh"))
-    cleanTable(graphdb)
+    driver = GraphDatabase.driver(uri="bolt://localhost:7687" , auth=("neo4j", "eheh"))
+    cleanTable(driver)
 
 
     text = ""
     # Reads file
-    for line in fileinput.input(file):
+    for line in fileinput.input(resto):
         text += line
 
-    lastPerson = ""
+
+    if "-f" in dop:
+        createNameList(resto)
+
     phrases = []
     phrases = formatText(text)
     p = []
     pM = r"(?:[A-ZÀ-Ü]\w+|[A-ZÀ-Ü]\.|[A-ZÀ-Ü][a-z]\.)"
-    rS = r"(?:casado|casou|divorciado|divorciou|namorado|namorada|esposa|marido|não são casados|namora)"
+    d = r"(?:da|do|de|dos|das)"
+    nP = f"{pM}(?: {pM}| {d} {pM})*"
+    rS = r"(?:cas(?:ado|ada|ou)|divorci(?:ado|ada|ou|aram)|namor(?:a|ou|aram|ada|ado)|esposa|marido|não são casados|irmãos)"
 
-    #Equitecar nomes e relationStatus em cada frase
+    #Tags names and relationships on each phrase
     for phrase in phrases:
-        ph = re.sub(r"(?:(o|a|O|A|do|da)) " + f"({pM})", r' {\1}:s {\2}:p', phrase)
+        ph = re.sub(r"(?:o|a|O|A|do|da) " + f"({nP})", r'{\1}:p', phrase)
         ph = re.sub(f" ({rS})", r'{\1}:r', ph)
         p.append(ph)
-        #print(p)
 
+    #iterates each phrase
     for phrase in p:
-
+        #looks for a relation
         if(re.search(r"{(.*?)}:r", phrase)):
-            #relation = re.findall(r"{(\w+)}:r", phrase)
             relation = getRelation(phrase)
+            #looks for subjects, needs a minimal of two to work
             if(re.search(r"(.*?):p", phrase)):
-                result = re.findall(r"{(\w+)}:p",phrase)
+                result = re.findall(r"{(\w+(?: \w+)*)}:p",phrase)
                 if(len(result) > 1):
-                    createNode(graphdb,result[0])
-                    createNode(graphdb,result[1])
-                    addRelation(graphdb, result[0], result[1], relation)
-                    #print(f"Pessoa 1: {result[0]} Pessoa 2: {result[1]} Estado: {relation}")
-                #elif len(result > 2):
-            lastPerson = result[len(result) - 1]    
+                    createNode(driver,result[0])
+                    createNode(driver,result[1])
+                    addRelation(driver, result[0], result[1], relation)
+                #if there's more than 2 subjects we assume they are all related
+                if(len(result) > 2):
+                    for i in range(0, len(result)):
+                        createNode(driver, result[i])
+                        if not i is 0:
+                            for x in range(0,i):
+                                addRelation(driver, result[x],result[i],relation)
 
 if __name__ == "__main__":
-    main()
+    main()  
