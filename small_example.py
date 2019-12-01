a5e3952d8afd1ffb76866562c4e0e87d4d08c8f5
Marcio Rocha <marciosr@live.com.pt>
2019-12-01 17:17:35 +0000

    new small example

--- small_example.py
+++ small_example.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 from neo4j import GraphDatabase #import of neo4j
 
-def createNode(driver, t, name):
-    with driver.session() as session:
-        session.run("CREATE (node:"+t+" {name: $name}) RETURN node.name",t = t, name = name)
-            
-def createRelation(driver, n1, n2, r):
-    with driver.session() as session:
-        session.run("MATCH (node1:Warehouse {name: $n1}),(node2:Store {name: $n2}) MERGE (node1)-[r:"+r+"]->(node2) RETURN node1.name,type(r),node2.name",n1=n1,n2=n2,r=r)
-
 def main():
     driver = GraphDatabase.driver(uri="bolt://localhost:7687" , auth=("neo4j", "eheh"))
-    
-    createNode(driver,"Store","Lewis")
-    createNode(driver,"Warehouse","Clothing")
-    createRelation(driver,"Clothing","Lewis","Provides")
+        
+    warehouse = "Clothing"
+    store = "Lewis"
+    relation = "Provides"
 
+    with driver.session() as session:
+        session.run("CREATE (node1:Warehouse {name: $warehouse})-[r:"+relation+"]->(node2:Store {name: $store})",warehouse=warehouse,store=store,relation=relation)
+        
 if __name__ == "__main__":
     main()
